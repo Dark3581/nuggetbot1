7ed2b6beac51d59f9828a1be6b86a4e1e48d871c
Dark3581 <ifndark@gmail.com>
2020-12-06 03:30:54 -0800

    Lol

--- index.js
+++ index.js
@@ -74,8 +74,9 @@ client.on("message", async(message) => {
             let result = await searcher.search(args.join(" "), { type: "video" })
         if(result.first === null || result.totalResults === 0) 
                 return message.channel.send(":x: **No results**")
-        if(message.guild.voiceConnection === null)
-        return queue.delete(message.guild.id);
+            let vc1  = message.guild.voiceConnection;
+        if(!vc1){
+        return queue.delete(message.guild.id);}
             const songInfo = await ytdl.getInfo(result.first.url)
 
             let song = {
