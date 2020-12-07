4659ce648a07ed30c97b99f5673a6a969ae2ce36
Dark3581 <ifndark@gmail.com>
2020-12-07 03:10:44 -0800

    Added

--- commands/play.js
+++ commands/play.js
@@ -72,7 +72,7 @@ module.exports.run = async (client, message, args, queue, searcher ) => {
             let msg = new Discord.MessageEmbed()
                 .setTitle('**Added**')
                 .addField(song.title, '\u200B')
-                .addField('Duration: ', song.vLength)
+                .addField(`Duration: ${song.vLength}`, )
                 .setThumbnail(song.thumbnail)
             return message.channel.send(msg);  
             }
