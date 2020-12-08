7c21108a04105d0436ed12ec1650bd4661017f0b
Dark3581 <ifndark@gmail.com>
2020-12-08 02:09:40 -0800

    added

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 const ytdl = require('ytdl-core');
 const ytpl = require('ytpl')
-const userl = user.displayAvatarURL()
 module.exports.run = async (client, message, args, queue, searcher ) => {
     const vc = message.member.voice.channel;
     if(!vc)
@@ -82,7 +81,7 @@ module.exports.run = async (client, message, args, queue, searcher ) => {
         }else{
             serverQueue.songs.push(song);
             if(playlist) return undefined
-    
+            const userl = user.displayAvatarURL()
         
             let msg = new Discord.MessageEmbed()
                 .setTitle('**Added**')
