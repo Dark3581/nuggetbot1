e99ffa003051572155b01ac3850a39515e5c9dd2
Dark3581 <ifndark@gmail.com>
2020-12-08 14:51:51 -0800

    added

--- commands/queue.js
+++ commands/queue.js
@@ -5,7 +5,16 @@ module.exports.run = (client, message, args, queue, searcher ) => {
             return message.channel.send("There is no music currently playing!");
         if(message.member.voice.channel != message.guild.me.voice.channel)
             return message.channel.send("You are not in the voice channel!")
-                    
+        if (parseInt(args[1]) < 1 )
+            return message.channel.send('There is nothing to remove on that position')
+        switch(args[0].toLowerCase()){
+                case 'remove':
+                    serverQueue.songs.splice( parseInt(args[0]), 1)
+                    if (serverQueue.songs.length < parseInt(args[0]))
+                        return message.channel.send("there is no song on that number")
+                    break;}
+
+            console.log(serverQueue.songs)
         let nowPlaying = serverQueue.songs[0];
         let qMsg =  `${nowPlaying.title}\n \n`
         let lMsg = ''
