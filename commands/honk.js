f8289086676da28d7dbc05917656d1cac48ed5cf
Dark3581 <ifndark@gmail.com>
2020-12-08 23:55:34 -0800

    added

---
+++ commands/honk.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+module.exports.run = (client, message, args, queue, searcher) =>{
+message.channel.send('honk')
+
+}
+
+module.exports.config = {
+    name: "honk",
+    aliases: ['nigger',]}
