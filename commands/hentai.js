293b3e27f0e2ea0afec0200dd3f3f87ca6c9f371
Dark3581 <ifndark@gmail.com>
2020-12-11 13:20:42 -0800

    added

--- commands/hentai.js
+++ commands/hentai.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 module.exports.run = async(client, message, args, queue, searcher,   ) => {
+if(message.channel.id === '739002385531404288'){
    
 fetch('https://meme-api.herokuapp.com/gimme/hentai')
 .then(res => res.json())
@@ -12,6 +13,10 @@ fetch('https://meme-api.herokuapp.com/gimme/hentai')
     message.channel.send(memeEmbed)
     
 });
+
+}else{
+    return message.channel.send(':x: Wrong channel')
+}
 
 }
 
