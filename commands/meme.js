7da94fd9ca5ce55d0742d89af9b1cd4cce06bc28
Dark3581 <ifndark@gmail.com>
2020-12-11 11:56:56 -0800

    added

--- commands/meme.js
+++ commands/meme.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 module.exports.run = async(client, message, args, queue, searcher,   ) => {
-let msg = await message.channel.send('Fetching meme');
+let msg = await message.channel.send('c:');
 fetch('https://meme-api.herokuapp.com/gimme')
 .then(res => res.json())
 .then(json => {
