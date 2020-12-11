01775fef6ce1c944077fa44f484470634ed88b62
Dark3581 <ifndark@gmail.com>
2020-12-11 12:23:32 -0800

    added

--- commands/meme.js
+++ commands/meme.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 module.exports.run = async(client, message, args, queue, searcher,   ) => {
+   
 let msg = await message.channel.send('c:');
-fetch('https://meme-api.herokuapp.com/gimme')
+fetch('https://meme-api.herokuapp.com/gimme/hentai')
 .then(res => res.json())
 .then(json => {
     let memeEmbed = new Discord.MessageEmbed()
@@ -10,12 +11,13 @@ fetch('https://meme-api.herokuapp.com/gimme')
     .setImage(json.url)
     .setFooter(`Link: ${json.postLink} | Subreddit: ${json.subreddit}`)
     msg.edit(memeEmbed)
+    
 });
 
 }
 
 module.exports.config ={
-    name: 'meme',
-    aliases: ['m']
+    name: 'hentai',
+    aliases: [';)']
 
 }
