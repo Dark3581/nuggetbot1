0b71c2db93467a3bf3650b4f71a33248106e5a48
Dark3581 <ifndark@gmail.com>
2020-12-12 01:32:07 -0800

    added

--- commands/subreddit.js
+++ commands/subreddit.js
@@ -39,6 +39,27 @@ module.exports.run = async(client, message, args, queue, searcher,   ) => {
             }else{
                 return
             }break;
+            case 'buttfangs':
+            if(message.channel.id === '739002385531404288'||
+            message.channel.id === '646849145289834506'||
+            message.channel.id === '785079847763574794'||
+            message.channel.id === '782891383361896469'||
+            message.channel.id === '784417039425994772'){
+            fetch('https://meme-api.herokuapp.com/gimme/buttfangs')
+            .then(res => res.json())
+            .then(json => {
+                let buttEmbed = new Discord.MessageEmbed()
+                .setTitle(json.title)
+                .setImage(json.url)
+                .setFooter(`Link: ${json.postLink} | Subreddit: ${json.subreddit}`)
+                message.channel.send(buttEmbed)
+                
+            })
+            
+            }else{
+                return
+            }break;
+
             case 'meme':
                  if(message.channel.id === '636641555913900034'||
         message.channel.id === '784417039425994772'||
